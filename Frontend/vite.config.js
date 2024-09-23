6af26b97028104ed1778d9f0733bb8782d164488
Rohitsingh667 <rohitraa45@gmail.com>
2024-09-23 10:29:29 +0530

    Cloud connected

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    entry: 'src/main.jsx',
+    outDir: 'build',
+  },
   server: {
     fs: {
       allow: ['..', 'node_modules/slick-carousel/slick/fonts'], // Add directories here
